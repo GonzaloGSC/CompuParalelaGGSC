e21f16c46fb8e7dc0684dc90bf6bc056c0903a7b
gonzalosalinas <63922626+GonzaloGSC@users.noreply.github.com>
2020-08-22 13:35:46 -0400

    Agregado manual, taller 2

---
+++ Taller2/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=cbase
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/cbase
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=cbase.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/cbase.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=cbase
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/cbase
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=cbase.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/cbase.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
